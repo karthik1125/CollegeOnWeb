8aed1b2721d66969892b3afaa2483038aef4a3d7
karthik1125 <subaskarthikeya.m16@iiits.in>
2018-10-25 23:30:18 +0530

    added datepicker along with time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import { Button,Icon } from 'antd';
 import SlideBar from './Sidebar';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 import logo from './assets/logo.png';
 import './App.css';
 
 
+const theme = createMuiTheme({
+  overrides: {
+    // Name of the component ⚛️ / style sheet
+    MuiButton: {
+      // Name of the rule
+      root: {
+        // Some CSS
+        background: 'linear-gradient(45deg, #f5f9d9 20%, #563947 40%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
+});
 
 class App extends Component {
   constructor(props){
@@ -20,7 +39,7 @@ class App extends Component {
   }
 
   handleMenu = () => {
-    console.log("SDJkaSJDKJSAD")
+
     this.setState({clicked:false,})
   }
   render() {
@@ -40,7 +59,12 @@ class App extends Component {
               <img src={logo} className="App-logo" alt="logo" />
               </div>
               <br></br>
-              <Button type="primary" style={{width:160,height:60,backgroundColor:"#563947",borderRadius:10,color:"#f5f9d9"}} onClick={() => this.handleClick()} block >Login</Button>
+
+              <MuiThemeProvider theme={theme}>
+                <Button onClick={() => this.handleClick()}>Login</Button>
+              </MuiThemeProvider>
+
+              {/* <Button type="primary" style={{width:160,height:60,backgroundColor:"#563947",borderRadius:10,color:"#f5f9d9"}} onClick={() => this.handleClick()} block >Login</Button> */}
             </center>
             </header>
           </div>)}
